aa532d45e00469198e457bbfa03cdf6bf84bbe55
joaogabrieldeassis <joaoassisgabriel@gmail.com>
2022-06-26 08:33:28 -0300

    Finalizando os testes

--- STORE/Store.Tests/Entities/OrderTests.cs
+++ STORE/Store.Tests/Entities/OrderTests.cs
@@ -14,37 +14,49 @@ public class OrderTests
     [TestCategory("Domain")]
     public void Dado_um_novo_Pedido_valido_ele_deve_gerar_um_numero_de_8_caracters()
     {
+        var orderNumber = new Order(_customer, 0, null);
+        Assert.AreEqual(8, orderNumber.Number.Length);
 
-        var order = new Order(_customer, 0, null);
-
-        Assert.AreEqual(8, order.Number.Length);
     }
     [TestMethod]
     [TestCategory("Domain")]
     public void Dado_um_novo_Pedido_seu_status_deve_ser_aguardando_pagamento()
     {
-        var item = new Order(_customer, 10, _discount);
-        Assert.Equals(item.Status, EOrderStatus.WaitingPayment);
-
+        var payment = new Order(_customer, 0, null);
+        Assert.AreEqual(payment.Status, EOrderStatus.WaitingPayment);
     }
     [TestMethod]
     [TestCategory("Domain")]
     public void Dado_um_pagamento_do_Pedido_seu_status_deve_ser_aguardando_entrega()
     {
-        var delivery = new Order(_customer, 10, _discount);
-        delivery.AddItem(_order, 2);
-        delivery.Total();
-        delivery.Pay(100);
-        Assert.Equals(delivery.Status, EOrderStatus.WaitingDelivery);
+        var delivery = new Order(_customer, 0, null);
+        delivery.AddItem(_order, 3);
+        delivery.Pay(300);
+        Assert.AreEqual(delivery.Status, EOrderStatus.WaitingDelivery);
     }
 
     [TestMethod]
     [TestCategory("Domain")]
     public void Dado_um_novo_Pedido_cancelado_seu_status_deve_ser_cancelado()
     {
-        var canceled = new Order(_customer, 10, null);
-        Assert.Equals(canceled.Status, EOrderStatus.Canceled);
-
+        var canceled = new Order(_customer, 0, null);
+        canceled.Cancel();
+        Assert.AreEqual(canceled.Status, EOrderStatus.Canceled);
     }
-
+    [TestMethod]
+    [TestCategory("Domain")]
+    public void Dado_um_novo_Item_sem_Produto_o_mesmo_nao_deve_ser_adicionado()
+    {
+        var canceled = new Order(_customer, 0, null);
+        canceled.AddItem(null, 0);
+        Assert.AreEqual(canceled.Itemns.Count, 0);
+    }
+    [TestMethod]
+    [TestCategory("Domain")]
+    public void Dado_um_novo_Item_com_quantidade_0_ou_menor_o_mesmo_nao_deve_ser_adicionado()
+    {
+        var canceled = new Order(_customer, 0, null);
+        canceled.AddItem(null, 0);
+        Assert.AreEqual(canceled.Itemns.Count, 0);
+    }
 }
